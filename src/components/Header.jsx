601035eb7ec7aa8deae3c389f7a5b3595a4f13bb
AsmaaMoallim <asmaaam1o1@gmail.com>
2022-01-08 13:42:25 +0300

    header and button components done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,25 @@
 // used to be required but not any more.. espesially when we use classes
 // import React from 'react'
+import Button from "./Button";
 
-//destructre the props {} and just take whhat you need... without the messy props.something 
-const Header = ({title}) => {
-    return (
-        <header>
-            <h1>Hello from React
-            </h1>
-                 {title}
+//destructre the props {} and just take whhat you need... without the messy props.something
+const Header = ({ title }) => {
+    const handelClick = () => {
+        console.log("Clicked")
+    }
 
-        </header>
-    )
-}
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      <Button color="green" text="Add" onClick={handelClick}/>
+    </header>
+  );
+};
 
 
-// we can have a default props value if not passed down from the parent to the chiled 
-
+// we can have a default props value if not passed down from the parent to the chiled
 Header.defaultProps = {
-    title : "just learning"
-}
+  title: "just learning",
+};
 
-
-export default Header
+export default Header;
